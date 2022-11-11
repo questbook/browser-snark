f65cb6506e3d59f44ac2e0a2a9f5e43da5dcf1e4
madhavanmalolan <madhavan93@gmail.com>
2022-11-11 03:16:49 -0800

    Publishing as standalone library

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Preimage } from '../browser-snark'
 import { ethers } from "ethers"
 
+const { Preimage } = require('browser-snark');
 
 export default function Home() {
 
